eeff1e9b8798bbba4155a33afe7b151f033741c4
donovoi <michael_john55@hotmail.com>
2019-09-25 12:01:10 +1000

    Updated Gradebook enums

---
+++ GradeBook/GradeBooks/StandardgradeBook.cs
@@ -0,0 +1,11 @@
+﻿
+namespace GradeBook.GradeBooks
+{
+    public class StandardgradeBook : BaseGradeBook
+    {
+        public StandardgradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
